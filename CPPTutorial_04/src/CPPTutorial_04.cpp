a2fd71ab9d6f1ca668e746b35c820d32a598c5bb
RayAndrade <TheRayCode@RayAndrade.com>
2020-02-08 15:44:13 -0800

    part 3 of Banas tutorial

--- CPPTutorial_04/src/CPPTutorial_04.cpp
+++ CPPTutorial_04/src/CPPTutorial_04.cpp
@@ -4,18 +4,49 @@
 // Version     :
 // Copyright   : Your copyright notice
 // Description : C++ Tutorial - Derek Banas 33:00
+// http://www.newthinktank.com/2018/03/c-tutorial-26/
+// https://youtu.be/N5HgK1bTLOg
 //============================================================================
 
 #include <iostream>
-//#include <cstdlib>
+// #include <cstdlib>
 
-//#include <string>
-//#include <vector>
-//#include <sstream>
+// #include <string>
+// #include <vector>
+// #include <sstream>
 #include <limits>
 
 
 int main() {
+	std::string sQuestion ("Enter Number 1 : ");
+	std::string sNum1, sNum2;
+	std::cout << sQuestion;
 
+	getline(std::cin, sNum1);
+	std::cout << "Enter Number 2 : ";
+	getline(std::cin, sNum2);
+
+    int nNum1 = std::stoi(sNum1);
+    int nNum2 = std::stoi(sNum2);
+
+    printf("%d + %d = %d\n",nNum1, nNum2,
+    		(nNum1 + nNum2));
+
+    printf("%d - %d = %d\n",nNum1, nNum2,
+        		(nNum1 - nNum2));
+
+    printf("%d * %d = %d\n",nNum1, nNum2,
+        		(nNum1 * nNum2));
+
+    printf("%d / %d = %d\n",nNum1, nNum2,
+        		(nNum1 / nNum2));
+
+
+    printf("%d %% %d = %d\n",nNum1, nNum2,
+        		(nNum1 % nNum2));
+
+
+
+	// 28:00
 	return 0;
 }
